0db5a172b625f69422dc97d80b397fd2a9a4e578
Kai Wen <taykaiwen1111@gmail.com>
2026-03-15 01:26:34 +0800

    tutorial done

--- Timer/Program.cs
+++ Timer/Program.cs
@@ -14,15 +14,25 @@ namespace Timer
             Timer timer = new Timer();
 
             // create some alarms
-            TextAlarm alarm1 = new TextAlarm("Wake up!");
-            BeepAlarm alarm2 = new BeepAlarm();
+            TextAlarm alarm1 = new TextAlarm("First Alarm!");
+            TextAlarm alarm2 = new TextAlarm("Second Alarm!");
+
+            System.Console.Write("Enter number of ticks: ");
+            string input = System.Console.ReadLine();
+            int numberOfTicks;
+
+            if (!int.TryParse(input, out numberOfTicks) || numberOfTicks <= 0)
+            {
+                System.Console.WriteLine("Invalid input. Using 3 ticks.");
+                numberOfTicks = 3;
+            }
 
             // attach the alarms to the clock
             timer.Attach(alarm1);
             timer.Attach(alarm2);
 
-            // run the clock for three seconds
-            timer.Run(3);
+            // run the clock for the requested number of ticks
+            timer.Run(numberOfTicks);
         }
     }
 }
